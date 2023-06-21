0e5e55837aa7e1f82323291b4eaf30e476760c4e
Ben Brook <hellobenbrook@protonmail.com>
2023-06-21 20:30:27 +0900

    Schema and .gitignore update

--- schema.sql
+++ schema.sql
@@ -1,9 +1,12 @@
 -- npx wrangler d1 execute dev-db-classes --file=./schema.sql --local
--- npx wrangler d1 execute dev-db-classes --command='SELECT * FROM UserHistory' --local
+-- npx wrangler d1 execute dev-db-classes --command='SELECT * FROM products' --local
+-- npx wrangler d1 execute dev-db-classes --command='SELECT * FROM userhistory' --local
+
+-- npx wrangler d1 execute dev-db-classes --file=./schema.sql
+-- npx wrangler d1 execute dev-db-classes --command='SELECT * FROM products'
+-- npx wrangler d1 execute dev-db-classes --command='SELECT * FROM userhistory'
 DROP TABLE IF EXISTS products;
 CREATE TABLE IF NOT EXISTS products (productid INTEGER PRIMARY KEY, classification TEXT, lastupdated INTEGER);
-INSERT INTO products VALUES ('1', 'clothing', 1687240430), ('2', 'food', 1687240430);
 
 DROP TABLE IF EXISTS userhistory;
 CREATE TABLE IF NOT EXISTS userhistory (cookie TEXT, productid TEXT, lastvisited INTEGER, PRIMARY KEY (cookie, productid));
-INSERT INTO userhistory VALUES ('example-cookie', '2', 1687311878), ('example-cookie', '1', 1687311913);
